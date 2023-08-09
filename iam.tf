2ea0b00fc5f00392e0f686fe87cbc7e9ab3d4065
imccarty426 <111451071+imccarty426@users.noreply.github.com>
2023-08-08 22:08:20 -0400

    upload

--- iam.tf
+++ iam.tf
@@ -3,13 +3,13 @@ resource "aws_key_pair" "wiztt_ssh" {
   public_key = file("./keys/wiztt_public.pub")
 }
 # Creates the Policy for the Wiz tech task with permissions to create and delete VMs
-resource "aws_iam_policy" "wiztt_create_delete_vm_policy" {
+resource "aws_iam_policy" "wiztt_ec2full" {
   name = "VMPolicy"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Action = ["ec2:RunInstances", "ec2:TerminateInstances"],
+        Action = ["ec2:*"],
         Effect = "Allow",
         Resource = "*"
       }
@@ -33,7 +33,7 @@ resource "aws_iam_role" "wiztt_mongodb_vm_role" {
     ]
   })
 }
-resource "aws_iam_role_policy_attachment" "wiztt_create_delete_vm_policy_attachment" {
-  policy_arn = aws_iam_policy.wiztt_create_delete_vm_policy.arn
+resource "aws_iam_role_policy_attachment" "wiztt_ec2full_attachment" {
+  policy_arn = aws_iam_policy.wiztt_ec2full.arn
   role       = aws_iam_role.wiztt_mongodb_vm_role.name
 }
--- s3.tf
+++ s3.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "mongodb_backup" {
   bucket = "mongodb_backup"  
   acl    = "public-read"
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
